052b963e1bee4c5f76ae84eee313bdc120e80b94
Anil_Ramcharran <anil.gitme@gmail.com>
2021-01-13 20:05:04 -0500

    created state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
+
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  state = [{
+    name: '',
+    id: '',
+    completed: false
+  }]
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  addNewTask = newTaskName => {
+    const newState = {
+      ...this.state,
+       taskList: [
+         ...this.state.taskList,
+         {
+           name: newTaskName,
+           completed: false,
+           id: Date.now()
+         }
+       ]
+    }
+    this.setState(newState)
+  };
+
+  
+
   render() {
     return (
       <div>
